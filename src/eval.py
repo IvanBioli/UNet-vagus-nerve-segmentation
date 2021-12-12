7d8c88204aac27610f9b6b811af4dff0904c3610
umerhasan17 <umerhasan17@gmail.com>
2021-12-12 21:49:02 +0100

    Move functions out of run.py

--- src/eval.py
+++ src/eval.py
@@ -50,9 +50,28 @@ def model_iou(trained_model, test_img_generator, test_anno_generator, show_image
     if show_images:
         for i in range(batch_size):
             # show_combined_result(model_input=x, y_true=y_true, y_pred=y_pred, i=i, iou_score=iou_score, save_file='results/combined1.png')
-            show_overlay_result(model_input=x, y_true=y_true, y_pred=y_pred, i=i, iou_score=iou_score, save_file='results/overlay2.png')
+            show_overlay_result(model_input=x, y_true=y_true, y_pred=y_pred, i=i, iou_score=iou_score,
+                                save_file='results/overlay2.png')
             # show_original_image(x, i=i)
             # show_original_annotation(y_true, i=i)
             # show_predicted_annotation(y_pred, i=i)
 
     return iou_score
+
+
+def output_predictions(trained_model=None, trained_model_id=None):
+    if trained_model is None and trained_model_id is None:
+        raise ValueError('Must supply either model or model id to output predictions.')
+
+    if trained_model_id is not None:
+        trained_model = keras.models.load_model('model_checkpoints/trained_model_id.h5')
+
+    # TODO call prediction code from notebook here as well
+    print('Generating predictions')
+    one_prediction_iou(
+        trained_model,
+        test_img=np.load(
+            'data/vagus_dataset_10/validation/images/vago DX - 27.06.18 - HH - vetrino 1 - prox - campione 0002.npy'),
+        test_anno=np.load(
+            'data/vagus_dataset_10/validation/annotations/vago DX - 27.06.18 - HH - vetrino 1 - prox - campione 0002.npy')
+    )
---
+++ src/train.py
@@ -0,0 +1,53 @@
+import pickle
+
+from tensorflow import keras
+
+from src.config import num_classes, batch_size, img_size, epochs
+from src.data_loader import VagusDataLoader
+from src.loss import SparseMeanIoU, dice_loss, nerve_segmentation_loss
+
+
+def train(model, model_id, train_img_target_pairs, val_img_target_pairs):
+    _optimizer = keras.optimizers.Adam()
+    _loss = nerve_segmentation_loss
+    # _loss = keras.losses.SparseCategoricalCrossentropy()
+    model.compile(
+        optimizer=_optimizer,
+        loss=_loss,
+        metrics=[
+            SparseMeanIoU(num_classes=num_classes, name='sparse_mean_iou'),
+            dice_loss,
+            keras.metrics.SparseCategoricalAccuracy(),
+            keras.metrics.SparseCategoricalCrossentropy()
+        ]
+    )
+
+    callbacks = [
+        keras.callbacks.ModelCheckpoint(f'model_checkpoints/{model_id}.h5', save_best_only=True)
+    ]
+
+    (img_paths, target_paths) = train_img_target_pairs
+    assert len(img_paths) == len(target_paths)
+
+    print('Training')
+
+    train_x, train_y = img_paths, target_paths
+    val_x, val_y = val_img_target_pairs
+
+    assert len(train_x) == len(train_y)
+    assert len(val_x) == len(val_y)
+
+    print(f'Create train dataset with batch_size={batch_size}, img_size={img_size}, n={len(train_x)}')
+    train_data = VagusDataLoader(batch_size, img_size, train_x, train_y)
+    print(f'Create validation dataset with batch_size={batch_size}, img_size={img_size}, n={len(val_x)}')
+    val_data = VagusDataLoader(batch_size, img_size, val_x, val_y)
+
+    # Fit to current train and validation split
+    model_history = model.fit(train_data, epochs=epochs, validation_data=val_data, callbacks=callbacks)
+
+    print('Finished training')
+
+    with open(f'model_losses/{model_id}.pkl', 'wb') as results_file:
+        pickle.dump(model_history.history, results_file)
+
+    return model
